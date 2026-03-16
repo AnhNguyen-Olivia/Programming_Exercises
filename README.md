84efb3dd6d41b14f4fcf5bb759e84cd29e93c104
AnhNguyen-Olivia <thuyanhnguyen.vietnam@gmail.com>
2026-03-16 20:17:28 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Programming_Exercises
+This is the place to hold codes for Programming Exercises class
---
+++ tttnew/src/main/java/tictactoe_new/App.java
@@ -0,0 +1,15 @@
+package tictactoe_new;
+
+public class App 
+{
+    public static void main( String[] args ){
+    switch(args[0]){
+        case "1" :
+            System.out.println("Human start\n"); break;
+        case "2":
+            System.out.println("Computer start\n"); break;
+        }
+
+        
+    }
+}
